b919a17233129d4fcf0cc69a2dafb9db17fb6d45
Kevin Glover <ktg@cs.nott.ac.uk>
2018-05-25 15:07:13 +0100

    OAuth example

--- src/main.js
+++ src/main.js
@@ -2,120 +2,165 @@
 const https = require('https');
 const express = require("express");
 const bodyParser = require("body-parser");
-const fs = require('fs');
-
+const oauth = require('oauth');
+const url = require('url');
 const databox = require('node-databox');
 
-var twitter = require('./twitter.js')();
+const twitter = require('./twitter.js')();
 
 let DefaultTwitConfig = {};
 try {
-  DefaultTwitConfig = require('./twitter-secret.json');
+	DefaultTwitConfig = require('./twitter-secret.json');
 } catch (e) {
-  DefaultTwitConfig = {};
+	DefaultTwitConfig = {};
 }
 
-const DATABOX_ZMQ_ENDPOINT = process.env.DATABOX_ZMQ_ENDPOINT
+const DATABOX_ZMQ_ENDPOINT = process.env.DATABOX_ZMQ_ENDPOINT;
 
 const credentials = databox.getHttpsCredentials();
 
-var PORT = process.env.port || '8080';
+const PORT = process.env.port || '8080';
 
-var HASH_TAGS_TO_TRACK = ['#raspberrypi', '#mozfest', '#databox', '#iot', '#NobelPrize'];
-var TWITER_USER = 'databox_mozfest';
+const HASH_TAGS_TO_TRACK = ['#raspberrypi', '#mozfest', '#databox', '#iot', '#NobelPrize'];
 
-var app = express();
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+function isProxied(req) {
+
+	return false;
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/ui', express.static('./src/www'));
 app.set('views', './src/views');
 app.set('view engine', 'pug');
 
-app.get('/ui', function(req, res) {
-  getSettings()
-    .then((settings)=>{
-      settings.hashTags = settings.hashTags.join(',');
-      console.log("[/ui render]");
-      res.render('index', settings);
-    })
-    .catch((error)=>{
-      console.log("[/ui] Error ",error);
-    });
+app.get('/ui', function (req, res) {
+	getSettings()
+		.then((settings) => {
+			console.log("[/ui render]");
+			res.render('index', settings);
+		})
+		.catch((error) => {
+			console.log("[/ui] Error ", error);
+		});
 });
 
-app.get('/ui/setCreds', function(req, res) {
+let consumer;
+app.post('/ui/login', (req, res) => {
+	getSettings()
+		.then((settings) => {
+			let urlRoot = req.body.callback;
+			if (urlRoot == null) {
+				urlRoot = "https://localhost/driver-twitter/ui/oauth"
+			}
+			if (req.body.consumer_key) {
+				settings.consumer_key = req.body.consumer_key;
+			}
+			if (req.body.consumer_secret) {
+				settings.consumer_secret = req.body.consumer_secret;
+			}
 
-  getSettings()
-    .then((settings)=>{
-      console.log("/ui/setCreds - setting == ", settings, res.query);
-      settings.consumer_key = req.param('consumer_key');
-      settings.consumer_secret = req.param('consumer_secret');
-      settings.access_token = req.param('access_token');
-      settings.access_token_secret = req.param('access_token_secret');
-      console.log("[NEW SETTINGS]",settings);
-      return setSettings(settings);
-    })
-    .then((settings)=>{
-      return Promise.all([twitter.connect(settings),Promise.resolve(settings)]);
-    })
-    .then((data)=>{
-      let T = data[0];
-      let settings = data[1];
-      stopAllStreams();
-      monitorTwitterEvents(T,settings);
-      res.status(200).send({statusCode:200, body:"ok"});
-    })
-    .catch((error)=>{
-      console.log("[setCreds] Error ",error);
-      res.status(400).send({statusCode:400, body:"error setting setCreds"});
-    });
+			const callback = urlRoot;
+			consumer = new oauth.OAuth(
+				"https://api.twitter.com/oauth/request_token", "https://api.twitter.com/oauth/access_token",
+				settings.consumer_key, settings.consumer_secret, "1.0A", callback, "HMAC-SHA1");
+			consumer.getOAuthRequestToken(function (error, oauthToken, oauthTokenSecret, results) {
+				if (error) {
+					res.send("Error getting OAuth request token : " + JSON.stringify(error), 500);
+				} else {
+					console.log(results);
+					settings.requestToken = oauthToken;
+					settings.requestTokenSecret = oauthTokenSecret;
+					settings.redirect = urlRoot;
+					console.log(settings);
+					setSettings(settings)
+						.then(() => {
+							res.render('oauth_redirect', {url: 'https://api.twitter.com/oauth/authenticate?oauth_token=' + settings.requestToken});
+						})
+				}
+			});
+		});
 });
 
-app.get('/ui/setHashTags', function(req, res) {
-    let newHashTags  = req.query.hashTags;
-    console.log(newHashTags);
-    getSettings()
-    .then((settings)=>{
-      settings.hashTags = newHashTags.split(',');
-      console.log("[SETTINGS]",settings);
-      return setSettings(settings);
-    })
-    .then((settings)=>{
-      return Promise.all([twitter.connect(settings),Promise.resolve(settings)]);
-    })
-    .then((data)=>{
-      let T = data[0];
-      let settings = data[1];
-      stopAllStreams();
-      monitorTwitterEvents(T,settings);
-      res.status(200).send({statusCode:200, body:"ok"});
-    })
-    .catch((error)=>{
-      console.log("[setHashTags] Error ",error);
-      res.status(400).send({statusCode:400, body:"error setting hashtags"});
-    });
+app.get('/ui/oauth', (req, res) => {
+	getSettings()
+		.then((settings) => {
+			consumer.getOAuthAccessToken(req.query.oauth_token, settings.requestTokenSecret, req.query.oauth_verifier, function (error, oauthAccessToken, oauthAccessTokenSecret, results) {
+				if (error) {
+					console.log(error);
+					res.send("Error getting OAuth access token : " + error + "[" + oauthAccessToken + "]" + "[" + oauthAccessTokenSecret + "]" + "[" + results + "]", 500);
+				} else {
+					settings.access_token = oauthAccessToken;
+					settings.access_token_secret = oauthAccessTokenSecret;
+					setSettings(settings)
+						.then(() => {
+							let redirectURL = settings.redirect;
+							redirectURL = redirectURL.replace('/ui/oauth', '/ui').replace('/driver-twitter', '/#!/driver-twitter');
+							res.render('redirect', {url: redirectURL});
+						});
+				}
+			});
+		});
+});
+
+app.post('/ui/logout', (req, res) => {
+	getSettings()
+		.then((settings) => {
+			settings.access_token = null;
+			settings.access_token_secret = null;
+			settings.redirect = req.body.callback;
+			stopAllStreams();
+			setSettings(settings)
+				.then(() => {
+					let redirectURL = settings.redirect;
+					redirectURL = redirectURL.replace('/ui/oauth', '/ui').replace('/driver-twitter', '/#!/driver-twitter');
+					res.render('redirect', {url: redirectURL});
+				});
+		});
+});
+
+
+app.get('/ui/setHashTags', function (req, res) {
+	let newHashTags = req.query.hashTags;
+	console.log(newHashTags);
+	getSettings()
+		.then((settings) => {
+			settings.hashTags = newHashTags.split(',');
+			console.log("[SETTINGS]", settings);
+			return setSettings(settings);
+		})
+		.then((settings) => {
+			return Promise.all([twitter.connect(settings), Promise.resolve(settings)]);
+		})
+		.then((data) => {
+			let T = data[0];
+			let settings = data[1];
+			stopAllStreams();
+			monitorTwitterEvents(T, settings);
+			res.render('index', settings);
+		})
+		.catch((error) => {
+			console.log("[setHashTags] Error ", error);
+			res.status(400).send({statusCode: 400, body: "error setting hashtags"});
+		});
 
 });
 
-app.get("/status", function(req, res) {
-    res.send("active");
+app.get("/status", function (req, res) {
+	res.send("active");
 });
 
 console.log("[Creating server]");
 https.createServer(credentials, app).listen(PORT);
 module.exports = app;
 
-
-var T = null;
-
-var vendor = "databox";
-
 let tsc = databox.NewTimeSeriesBlobClient(DATABOX_ZMQ_ENDPOINT, false);
 let kvc = databox.NewKeyValueClient(DATABOX_ZMQ_ENDPOINT, false);
 
 let timeLine = databox.NewDataSourceMetadata();
-timeLine.Description =  'Twitter user timeline data';
+timeLine.Description = 'Twitter user timeline data';
 timeLine.ContentType = 'application/json';
 timeLine.Vendor = 'Databox Inc.';
 timeLine.DataSourceType = 'twitterUserTimeLine';
@@ -123,7 +168,7 @@ timeLine.DataSourceID = 'twitterUserTimeLine';
 timeLine.StoreType = 'ts';
 
 let hashTag = databox.NewDataSourceMetadata();
-hashTag.Description =  'Twitter user hashtag data';
+hashTag.Description = 'Twitter user hashtag data';
 hashTag.ContentType = 'application/json';
 hashTag.Vendor = 'Databox Inc.';
 hashTag.DataSourceType = 'twitterHashTagStream';
@@ -173,156 +218,135 @@ driverSettings.StoreType = 'kv';
 
 
 tsc.RegisterDatasource(timeLine)
-.then(() => {
-  return tsc.RegisterDatasource(hashTag);
-})
-.then(() => {
-  return tsc.RegisterDatasource(userDM);
-})
-.then(() => {
-  return tsc.RegisterDatasource(userRetweet);
-})
-.then(() => {
-  return tsc.RegisterDatasource(userFav);
-})
-.then(() => {
-  return tsc.RegisterDatasource(testActuator);
-})
-.then(() => {
-  return kvc.RegisterDatasource(driverSettings);
-})
-.catch((err) => {
-  console.log("Error registering data source:" + err);
-});
+	.then(() => {
+		return tsc.RegisterDatasource(hashTag);
+	})
+	.then(() => {
+		return tsc.RegisterDatasource(userDM);
+	})
+	.then(() => {
+		return tsc.RegisterDatasource(userRetweet);
+	})
+	.then(() => {
+		return tsc.RegisterDatasource(userFav);
+	})
+	.then(() => {
+		return tsc.RegisterDatasource(testActuator);
+	})
+	.then(() => {
+		return kvc.RegisterDatasource(driverSettings);
+	})
+	.catch((err) => {
+		console.log("Error registering data source:" + err);
+	});
+
+let inlineSettings = DefaultTwitConfig;
+inlineSettings.hashTags = HASH_TAGS_TO_TRACK;
 
 getSettings()
-  .then((settings)=>{
-    console.log("Twitter Auth");
-    if(settings.hasOwnProperty('consumer_key')) {
-      return Promise.all([twitter.connect(settings),Promise.resolve(settings)]);
-    } else {
-      return Promise.all([Promise.resolve(null),Promise.resolve(settings)]);
-    }
-  })
-  .then((data)=>{
-    console.log("Connected to twitter!");
+	.then((settings) => {
+		console.log("Twitter Auth");
+		if (settings.hasOwnProperty('consumer_key')) {
+			return Promise.all([twitter.connect(settings), Promise.resolve(settings)]);
+		} else {
+			return Promise.all([Promise.resolve(null), Promise.resolve(settings)]);
+		}
+	})
+	.then((data) => {
+		console.log("Connected to twitter!");
 
-    let T = data[0];
-    let settings = data[1];
+		let T = data[0];
+		let settings = data[1];
 
-    //deal with the actuator
-    let datasourceid = "testActuator";
-    tsc.Observe(testActuator.DataSourceID, 0)
-    .catch((err)=>{
-      console.log("[Actuation observing error]",err);
-    })
-    .then((eventEmitter)=>{
-      eventEmitter.on('data',(data)=>{
-        console.log("[Actuation] data received ", data);
-      });
-    })
-    .catch((err)=>{
-      console.log("[Actuation error]",err);
-    });
+		// //deal with the actuator
+		// tsc.Observe(testActuator.DataSourceID, 0)
+		// 	.catch((err) => {
+		// 		console.log("[Actuation observing error]", err);
+		// 	})
+		// 	.then((eventEmitter) => {
+		// 		eventEmitter.on('data', (data) => {
+		// 			console.log("[Actuation] data received ", data);
+		// 		});
+		// 	})
+		// 	.catch((err) => {
+		// 		console.log("[Actuation error]", err);
+		// 	});
 
-    if(T != null) {
-      monitorTwitterEvents(T,settings);
-    }
+		if (T != null) {
+			monitorTwitterEvents(T, settings);
+		}
 
-  })
-  .catch((err) => {
-    console.log("[ERROR]",err);
-  });
+	})
+	.catch((err) => {
+		settings.access_token = null;
+		settings.access_token_secret = null;
+		setSettings(setSettings());
+		console.log("[ERROR]", err);
+	});
 
-var streams = [];
-const monitorTwitterEvents = (twit,settings)=>{
+let streams = [];
+const monitorTwitterEvents = (twit, settings) => {
 
-    console.log("monitorTwitterEvents called");
+	console.log("monitorTwitterEvents called");
 
-      //deal with twitter events
-    var HashtagStream = twit.stream('statuses/filter', { track: settings.hashTags , language:'en'});
-    streams.push(HashtagStream);
-    HashtagStream.on('tweet', function (tweet) {
-      save('twitterHashTagStream', tweet);
-    });
+	//deal with twitter events
+	const HashtagStream = twit.stream('statuses/filter', {track: settings.hashTags, language: 'en'});
+	streams.push(HashtagStream);
+	HashtagStream.on('tweet', function (tweet) {
+		save('twitterHashTagStream', tweet);
+	});
 
-    var UserStream = twit.stream('user', { stringify_friend_ids: true, with: 'followings', replies:'all' });
-    streams.push(UserStream);
-    UserStream.on('tweet', function (event) {
-      save('twitterUserTimeLine',event);
-    });
+	const UserStream = twit.stream('user', {stringify_friend_ids: true, with: 'followings', replies: 'all'});
+	streams.push(UserStream);
+	UserStream.on('tweet', function (event) {
+		save('twitterUserTimeLine', event);
+	});
 
-    UserStream.on('favorite', function (event) {
-      save('twitterFavorite',event);
-    });
+	UserStream.on('favorite', function (event) {
+		save('twitterFavorite', event);
+	});
 
-    UserStream.on('quoted_tweet', function (event) {
-      save('twitterRetweet',event);
-    });
+	UserStream.on('quoted_tweet', function (event) {
+		save('twitterRetweet', event);
+	});
 
-    UserStream.on('retweeted_retweet', function (event) {
-      save('twitterRetweet',event);
-    });
+	UserStream.on('retweeted_retweet', function (event) {
+		save('twitterRetweet', event);
+	});
 
-    UserStream.on('direct_message', function (event) {
-      save('twitterDirectMessage',event);
-    });
+	UserStream.on('direct_message', function (event) {
+		save('twitterDirectMessage', event);
+	});
 };
 
-function stopAllStreams () {
-  streams.map((st)=>{st.stop();});
-  streams = [];
+function stopAllStreams() {
+	streams.map((st) => {
+		st.stop();
+	});
+	streams = [];
 }
 
-function getSettings () {
-  datasourceid = 'twitterSettings';
-  return new Promise((resolve,reject)=>{
-    kvc.Read(datasourceid,"settings")
-    .then((settings)=>{
-      if(Object.keys(settings).length == 0) {
-         //return defaults
-       let settings = DefaultTwitConfig;
-       settings.hashTags = HASH_TAGS_TO_TRACK;
-       console.log("[getSettings] using defaults Using ----> ", settings);
-       resolve(settings);
-       return
-      }
-      console.log("[getSettings]",settings);
-      resolve(settings);
-    })
-    .catch((err)=>{
-     let settings = DefaultTwitConfig;
-     settings.hashTags = HASH_TAGS_TO_TRACK;
-     console.log("[getSettings] using defaults Using ----> ", settings);
-     resolve(settings);
-     return
-    });
-  });
- };
+function getSettings() {
+	return new Promise((resolve, reject) => {
+		resolve(inlineSettings);
+	});
+}
 
-function setSettings (settings) {
- let datasourceid = 'twitterSettings';
- return new Promise ((resolve,reject)=>{
-  kvc.Write(datasourceid, "settings", settings)
-    .then(()=>{
-      console.log('[setSettings] settings saved', settings);
-      resolve(settings);
-    })
-    .catch((err)=>{
-      console.log("Error setting settings", err);
-      reject(err);
-    });
-  });
-};
+function setSettings(settings) {
+	return new Promise((resolve, reject) => {
+		inlineSettings = settings;
+		resolve(inlineSettings);
+	});
+}
 
-function save (datasourceid,data) {
-  console.log("Saving tweet::", data.text);
-  json = {"data": data};
-  tsc.Write(datasourceid,data)
-  .then((resp)=>{
-    console.log("Save got response ", resp);
-  })
-  .catch((error)=>{
-    console.log("Error writing to store:", error);
-  });
+function save(datasourceid, data) {
+	console.log("Saving tweet::", data.text);
+	json = {"data": data};
+	tsc.Write(datasourceid, data)
+		.then((resp) => {
+			console.log("Save got response ", resp);
+		})
+		.catch((error) => {
+			console.log("Error writing to store:", error);
+		});
 }
--- src/twitter.js
+++ src/twitter.js
@@ -1,56 +1,50 @@
 /*jshint esversion: 6 */
 
-var Twit = require('twit');
-
-var T = null;
-
-var accessToken = null;
-var secret = null;
-var isSignedIn = false;
+const Twit = require('twit');
 
 module.exports = function () {
 
-  let isSignedIn = false;
+	let isSignedIn = false;
+	let T = null;
 
-  let T = null;
+	const connect = function (creds) {
+		console.log("Connecting to twitter!!");
 
-    var connect = function(creds) {
-      console.log("Connecting to twitter!!");
+		isSignedIn = false;
 
-      isSignedIn = false;
+		return new Promise((resolve, reject) => {
+			T = new Twit({
+				consumer_key: creds.consumer_key,
+				consumer_secret: creds.consumer_secret,
+				access_token: creds.access_token,
+				access_token_secret: creds.access_token_secret,
+				timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+			});
 
-      return new Promise((resolve, reject)=>{
-        T = new Twit({
-          consumer_key:         creds.consumer_key,
-          consumer_secret:      creds.consumer_secret,
-          access_token:         creds.access_token,
-          access_token_secret:  creds.access_token_secret,
-          timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-        })
+			T.get('account/verify_credentials', {}, function (err, data, response) {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(T);
+			})
+				.then(() => {
+					isSignedIn = true;
+					console.log('Creds OK');
+				})
+				.catch((err) => {
+					reject(err);
+					isSignedIn = false;
+				});
+		});
 
-        T.get('account/verify_credentials', { }, function (err, data, response) {
-          if(err) {
-            reject(err);
-            return;
-          }
-          resolve(T);
-        })
-        .then((result) => {
-          isSignedIn = true;
-          console.log('Creds OK');
-        })
-        .catch((err) => {
-          reject(err);
-          isSignedIn = false;
-        });
-      });
+	};
 
-    };
-
-    return {
-      'Twit':T,
-      'isSignedIn': function () { return isSignedIn;},
-      'connect':connect
-    };
-
+	return {
+		'Twit': T,
+		'isSignedIn': function () {
+			return isSignedIn;
+		},
+		'connect': connect
+	};
 };
